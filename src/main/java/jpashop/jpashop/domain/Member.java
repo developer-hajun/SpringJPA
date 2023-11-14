291b376ce8cc5c41647283ea46f5bf003ea84456
developer-hajun <juni8828@naver.com>
2023-11-14 19:17:13 +0900

    first commit

--- src/main/java/jpashop/jpashop/domain/Member.java
+++ src/main/java/jpashop/jpashop/domain/Member.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class Member {
     @Id
     @GeneratedValue
